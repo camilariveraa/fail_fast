c7ad53ed7631bbbb5f375e18782007a81b2bdfc6
Camila Rivera <c.riveraa2@uniandes.edu.co>
2024-11-30 19:43:06 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+const App: React.FC = () => {
+  return (
+    <div className="p-4">
+      <h1 className="text-xl md:text-3xl lg:text-5xl">
+        Responsive Text Size
+      </h1>
+    </div>
+  );
+};
+
+export default App;
