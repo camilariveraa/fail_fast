f77f1dcd3464eb45cdd0afadac17d447c98cc7b4
Juan Sebastian Gomez <156712607+jsgomezs1@users.noreply.github.com>
2024-11-30 20:20:02 -0500

    feat: :fire: I just created the NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
+import NavBar from "./components/navbar";
+
 const App: React.FC = () => {
   return (
-    <div className="p-4">
-      <h1 className="text-xl md:text-3xl lg:text-5xl">
-        Responsive Text Size
-        blaaaaaa
-      </h1>
-    </div>
+    <NavBar></NavBar>
   );
 };
 
---
+++ src/components/navbar.tsx
@@ -0,0 +1,11 @@
+const NavBar = () => {
+    return (  
+        <div>
+            <h2>
+                Hola Cami
+            </h2>
+        </div>
+    );
+}
+ 
+export default NavBar;
